84c3b95674e40e57aaff0b23b84e838ee3c71ace
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2022-06-22 22:23:49 +0200

    Commiting Homework for Class 04

---
+++ c4/Homework for Class 04/index.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+
+// Read a file
+fs.readFile("./file.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("Error", err);
+  }
+  console.log(data);
+});
+
+// Write a file
+fs.writeFile(
+  ".newFile.txt",
+  "Jimmy, Frank, Pulumbo, Anna,Arnold, Scott",
+  (err) => {
+    if (err) {
+      console.log("There seems to be an error", err);
+    }
+  }
+);
+
+// Rename a file
+fs.rename("file.txt", "hello.txt", (err) => {
+  if (err) {
+    console.log("There seems to be an error", err);
+  }
+});
+
+fs.rename("hello.txt", "file.txt", (err) => {
+  if (err) {
+    console.log("There seems to be an error", err);
+  }
+});
+
+// Copy a file
+
+fs.copyFile("colors.txt", "copiedFile.txt", (err) => {
+  if (err) {
+    console.log("There seems to be an error", err);
+  }
+  console.log("color.txt was copied to copiedFile.txt");
+});
+
+// Delete / Remove a file
+const path = "./file.txt";
+
+try {
+  fs.unlinkSync(path);
+} catch (err) {
+  console.error(err);
+}
