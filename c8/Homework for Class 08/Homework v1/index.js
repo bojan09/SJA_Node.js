07287983a34a9851e1ea75640e912fdf173ef896
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2022-07-09 18:53:58 +0200

    Added some comment to the code

---
+++ c8/Homework for Class 08/Homework v1/index.js
@@ -0,0 +1,19 @@
+// importing the express module
+const express = require("express");
+// importing studenti
+const studenti = require("./studenti");
+
+// creating an Express app and using it to listen to PORT 8080
+const app = express();
+const PORT = 8080;
+
+// activate the JSON(req.body) middleware/ it's a sort of a plugin
+app.use(express.json());
+
+// a route is defined for the / path that points to the studenti module.
+app.use("/", studenti);
+
+// when the server starts up, it prints out "Server running on port: http://localhost:8080".
+app.listen(PORT, () => {
+  console.log(`Server running on port: http://localhost:${PORT}`);
+});
